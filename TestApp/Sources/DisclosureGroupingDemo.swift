6f0a10a30a6d17d20ce6c55ca01c98c464fb0b6f
Alex Odawa <aodawa@mac.com>
2026-04-06 15:09:57 +0200

    Release 2026.04.06.3

--- TestApp/Sources/DisclosureGroupingDemo.swift
+++ TestApp/Sources/DisclosureGroupingDemo.swift
@@ -14,7 +14,7 @@ struct DisclosureGroupingDemo: View {
                     Toggle("Dark mode", isOn: $darkModeEnabled)
                 }
 
-                LabeledContent("Version", value: "2026.04.06.2")
+                LabeledContent("Version", value: "2026.04.06.3")
 
                 LabeledContent("Build", value: "42")
             }
