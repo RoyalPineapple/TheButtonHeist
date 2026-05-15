46174a880d87f1ab2944dc6effe0f8df573b55e3
Alex Odawa <aodawa@mac.com>
2026-05-15 16:03:48 +0200

    Bump parser package floor to 0.7.0

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .library(name: "ButtonHeist", targets: ["ButtonHeist"])
     ],
     dependencies: [
-        .package(url: "https://github.com/RoyalPineapple/AccessibilitySnapshotBH", from: "0.6.0"),
+        .package(url: "https://github.com/RoyalPineapple/AccessibilitySnapshotBH", from: "0.7.0"),
         .package(url: "https://github.com/apple/swift-certificates", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-crypto", from: "3.0.0"),
     ],
