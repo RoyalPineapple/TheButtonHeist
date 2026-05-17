d912e897d5cf2eef4c8fb5891bcda6c9565f4af3
Alex Odawa <aodawa@mac.com>
2026-05-17 18:40:57 +0200

    Release 0.3.4

--- Formula/buttonheist.rb
+++ Formula/buttonheist.rb
@@ -10,7 +10,7 @@
 class Buttonheist < Formula
   desc "Give AI agents full programmatic control of iOS apps"
   homepage "https://github.com/RoyalPineapple/TheButtonHeist"
-  version "0.3.3"
+  version "0.3.4"
 
   url "https://github.com/RoyalPineapple/TheButtonHeist/releases/download/v#{version}/buttonheist-#{version}-macos.tar.gz"
   sha256 "PLACEHOLDER"
