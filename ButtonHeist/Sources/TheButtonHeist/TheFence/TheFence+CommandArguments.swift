1d5149d85fe9a4386b72bf2422e547cd039ff456
Alex Odawa <aodawa@squareup.com>
2026-05-29 17:54:58 +0200

    Delete argument value rebuild hook (#920)

--- ButtonHeist/Sources/TheButtonHeist/TheFence/TheFence+CommandArguments.swift
+++ ButtonHeist/Sources/TheButtonHeist/TheFence/TheFence+CommandArguments.swift
@@ -23,10 +23,6 @@ extension TheFence {
         func dropping(_ key: String) -> CommandArgumentEnvelope {
             var values = argumentValues
             values.removeValue(forKey: key)
-            return withArgumentValues(values)
-        }
-
-        func withArgumentValues(_ values: [String: HeistValue]) -> CommandArgumentEnvelope {
             return CommandArgumentEnvelope(
                 values: values,
                 elementTarget: elementTarget,
@@ -44,17 +40,12 @@ extension TheFence {
             self.argumentValues = values
             self.argumentFieldPrefix = fieldPrefix
         }
-
-        func withArgumentValues(_ values: [String: HeistValue]) -> CommandArgumentObject {
-            CommandArgumentObject(values: values, fieldPrefix: argumentFieldPrefix)
-        }
     }
 
     protocol CommandArgumentReadable: Sendable {
         var argumentValues: [String: HeistValue] { get }
         var elementTarget: ElementTarget? { get }
         var argumentFieldPrefix: String? { get }
-        func withArgumentValues(_ values: [String: HeistValue]) -> Self
     }
 }
 
