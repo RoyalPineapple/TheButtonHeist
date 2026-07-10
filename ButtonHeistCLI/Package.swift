d9b03d2ee3fee2f76095c563acfaff323e89cba2
Alex Odawa <aodawa@mac.com>
2026-07-10 15:26:10 +0200

    Adopt AccessibilitySnapshotBH 0.23.0

--- ButtonHeistCLI/Package.swift
+++ ButtonHeistCLI/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     dependencies: [
         .package(name: "ButtonHeist", path: ".."),
-        .package(url: "https://github.com/RoyalPineapple/AccessibilitySnapshotBH", exact: "0.22.0"),
+        .package(url: "https://github.com/RoyalPineapple/AccessibilitySnapshotBH", exact: "0.23.0"),
         .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "1.7.0"))
     ],
     targets: [
