24308ec05aa8165b0287c77a935e49a19b68a278
Alex Odawa <aodawa@mac.com>
2026-07-13 13:04:19 +0200

    Compare live capture facts structurally

--- ButtonHeist/Sources/TheInsideJob/TheStash/LiveCapture.swift
+++ ButtonHeist/Sources/TheInsideJob/TheStash/LiveCapture.swift
@@ -425,19 +425,7 @@ struct LiveCapture: Equatable {
         _ lhs: AccessibilityElement,
         _ rhs: AccessibilityElement
     ) -> Bool {
-        if lhs == rhs { return true }
-
-        let encoder = JSONEncoder()
-        encoder.outputFormatting = [.sortedKeys]
-        encoder.nonConformingFloatEncodingStrategy = .convertToString(
-            positiveInfinity: "+Infinity",
-            negativeInfinity: "-Infinity",
-            nan: "NaN"
-        )
-        guard let lhsData = try? encoder.encode(lhs),
-              let rhsData = try? encoder.encode(rhs)
-        else { return false }
-        return lhsData == rhsData
+        lhs.matchesCapturedFacts(of: rhs)
     }
 
     private static func validateContainers(in tree: InterfaceTree) throws {
@@ -782,6 +770,124 @@ struct LiveCapture: Equatable {
 private extension TreePath {
     var liveCaptureDiagnosticDescription: String {
         "[\(indices.map(String.init).joined(separator: ", "))]"
+    }
+}
+
+private extension AccessibilityElement {
+    func matchesCapturedFacts(of other: AccessibilityElement) -> Bool {
+        description == other.description
+            && label == other.label
+            && value == other.value
+            && traits == other.traits
+            && identifier == other.identifier
+            && hint == other.hint
+            && userInputLabels == other.userInputLabels
+            && shape.matchesCapturedGeometry(of: other.shape)
+            && activationPoint.matchesCapturedGeometry(of: other.activationPoint)
+            && usesDefaultActivationPoint == other.usesDefaultActivationPoint
+            && customActions == other.customActions
+            && customContent == other.customContent
+            && customRotors.matchesCapturedFacts(of: other.customRotors)
+            && accessibilityLanguage == other.accessibilityLanguage
+            && respondsToUserInteraction == other.respondsToUserInteraction
+            && visibility == other.visibility
+    }
+}
+
+private extension Array where Element == AccessibilityElement.CustomRotor {
+    func matchesCapturedFacts(of other: Self) -> Bool {
+        count == other.count && zip(self, other).allSatisfy { lhs, rhs in
+            lhs.matchesCapturedFacts(of: rhs)
+        }
+    }
+}
+
+private extension AccessibilityElement.CustomRotor {
+    func matchesCapturedFacts(of other: Self) -> Bool {
+        name == other.name
+            && limit == other.limit
+            && resultMarkers.count == other.resultMarkers.count
+            && zip(resultMarkers, other.resultMarkers).allSatisfy { lhs, rhs in
+                lhs.matchesCapturedFacts(of: rhs)
+            }
+    }
+}
+
+private extension AccessibilityElement.CustomRotor.ResultMarker {
+    func matchesCapturedFacts(of other: Self) -> Bool {
+        guard elementDescription == other.elementDescription,
+              rangeDescription == other.rangeDescription else { return false }
+        switch (shape, other.shape) {
+        case (nil, nil):
+            return true
+        case let (.some(lhs), .some(rhs)):
+            return lhs.matchesCapturedGeometry(of: rhs)
+        case (.some, nil), (nil, .some):
+            return false
+        }
+    }
+}
+
+private extension AccessibilityShape {
+    func matchesCapturedGeometry(of other: Self) -> Bool {
+        switch (self, other) {
+        case let (.frame(lhs), .frame(rhs)):
+            return lhs.matchesCapturedGeometry(of: rhs)
+        case let (.path(lhs), .path(rhs)):
+            return lhs.count == rhs.count && zip(lhs, rhs).allSatisfy { lhs, rhs in
+                lhs.matchesCapturedGeometry(of: rhs)
+            }
+        case (.frame, .path), (.path, .frame):
+            return false
+        }
+    }
+}
+
+private extension AccessibilityPathElement {
+    func matchesCapturedGeometry(of other: Self) -> Bool {
+        switch (self, other) {
+        case let (.move(lhs), .move(rhs)),
+             let (.line(lhs), .line(rhs)):
+            return lhs.matchesCapturedGeometry(of: rhs)
+        case let (.quadCurve(lhs, lhsControl), .quadCurve(rhs, rhsControl)):
+            return lhs.matchesCapturedGeometry(of: rhs)
+                && lhsControl.matchesCapturedGeometry(of: rhsControl)
+        case let (.curve(lhs, lhsControl1, lhsControl2), .curve(rhs, rhsControl1, rhsControl2)):
+            return lhs.matchesCapturedGeometry(of: rhs)
+                && lhsControl1.matchesCapturedGeometry(of: rhsControl1)
+                && lhsControl2.matchesCapturedGeometry(of: rhsControl2)
+        case (.closeSubpath, .closeSubpath):
+            return true
+        default:
+            return false
+        }
+    }
+}
+
+private extension AccessibilityRect {
+    func matchesCapturedGeometry(of other: Self) -> Bool {
+        origin.matchesCapturedGeometry(of: other.origin)
+            && size.matchesCapturedGeometry(of: other.size)
+    }
+}
+
+private extension AccessibilityPoint {
+    func matchesCapturedGeometry(of other: Self) -> Bool {
+        x.matchesCapturedGeometry(of: other.x)
+            && y.matchesCapturedGeometry(of: other.y)
+    }
+}
+
+private extension AccessibilitySize {
+    func matchesCapturedGeometry(of other: Self) -> Bool {
+        width.matchesCapturedGeometry(of: other.width)
+            && height.matchesCapturedGeometry(of: other.height)
+    }
+}
+
+private extension Double {
+    func matchesCapturedGeometry(of other: Self) -> Bool {
+        self == other || (isNaN && other.isNaN)
     }
 }
 
